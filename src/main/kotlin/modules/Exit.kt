54be615f58529246c8c9a55f7331cc8a9994a418
PavelMizev <pma72amp@gmail.com>
2026-06-26 09:03:00 +0500

    En: Completed project; there may be bugs. Ru: Готовый проект, могут быть баги.

---
+++ src/main/kotlin/modules/Exit.kt
@@ -0,0 +1,33 @@
+package com.turashift.kotlincmd.modules
+
+fun exit(): Boolean {
+
+    //Exit val
+    var output: Boolean
+
+    //Confirmation request
+    print("Confirm exit? [Y/n] ")
+
+    //User confirmation
+    val confirm = readln()
+
+    //Action after input
+    when (confirm) {
+        "Y", "y" -> {
+            output = true
+        }
+        "N", "n" -> {
+            print("Exit cancelled")
+            output = false
+        }
+        else -> {
+            print("Exit cancelled due to user input error.")
+            output = false
+        }
+
+    }
+    //Data
+    return output
+}
+
+const val EXIT_COMMAND = "exit"
